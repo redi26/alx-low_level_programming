24cee9628d547c17a8109bd62172db1f671b7004
redi26 <phoenixmiss232.com>
2022-06-06 00:12:22 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  * main - prints exactly "Programming is like building a multilingual puzzle
- *
- * description: uses puts function to print lowercase letter
+ * Description: uses puts function to print lowercase letter
  * return: always 0
  */
 int main(void)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - prints exactly letter followed by new line
+ * Description: use the function printf
+ * Return: always 0
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n"); 
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
