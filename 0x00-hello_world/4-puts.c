29c9ceba09ea861babff32b3b9b7f14e8a3d3680
redi26 <phoenixmiss232.com>
2022-06-05 23:48:41 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-/*this is our multiple puzzle*/
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle
+ *
+ * description: uses puts function to print lowercase letter
+ * return: always 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
- }
+}
