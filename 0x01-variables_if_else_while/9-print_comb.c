0b0eca90c2db0e405123bcb8aec5feb643074329
redi26 <phoenixmiss232.com>
2022-06-06 06:50:38 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - use putchar function
+ * Description: program that prints all the possible combinations
+ * Return: always 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; n++)
+{
+putchar(ch);
+if (ch != 57)
+{
+
+putchar(44);
+putchar(32);
+}
+putchar(10);
+return (0);
+}
