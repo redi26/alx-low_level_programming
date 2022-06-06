edcb40f8929c8f5926f1d3844be8771fb8f62adf
redi26 <phoenixmiss232.com>
2022-06-06 04:57:11 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - use putchar function
+ * Description: program that prints the alphabet in lowercasse
+ * Return: always 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
+
+
