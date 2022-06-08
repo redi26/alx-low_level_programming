0e0d485c454614388020c792e6c3cfd65cebfd58
redi26 <phoenixmiss232.com>
2022-06-08 13:39:47 -0700

    print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,19 @@
  * void print_alphabet_x10(void) -> prints the lowercase alphabets
  *
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
-int j;
-int x;
-for (x = 0; x < 10; x++)
-{
-for (j = 'a'; j >= 'z'; j++)
-{
-_putchar(j);
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+
+	{
+		for (j = 'a'; j<= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		putchar('\n');
+	}
 }
-_putchar('\n');
-}
-}
+
