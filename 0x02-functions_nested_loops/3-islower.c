aac754af01fb10273e9d654c1bb38742506741c6
redi26 <phoenixmiss232.com>
2022-06-09 01:38:46 -0700

    isalpah

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _islower -> checks if the characte is lowercase
+ * _islower -> checks if the character c is lower
  * Return: returns 1 and 0 depending on condition
  */
 int _islower(int c)
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- *
- *
+ * _isalpha - function that checks for lowercase
+ * @c: returns 1 if c is lowercase
+ * Return: returns 0 or 1 depending on condition
  *
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z' ))
+	return ((c >= 'a' && c <= 'z'));
+}
